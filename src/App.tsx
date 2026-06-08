446770905319eb75d7511a9b40da8a49d83c1d77
David Navas <dn200499@gmail.com>
2026-06-08 16:14:22 -0500

    refactor: restructure App component and add conditional tab visibility for login route

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { IonApp, IonRouterOutlet, IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useLocation } from "react-router-dom";
 import { homeOutline, bookOutline, barbellOutline, terminalOutline, personOutline } from "ionicons/icons";
 import LoginPage from "./routes/login";
 import HomePage from "./routes/home";
@@ -17,49 +17,54 @@ function IndexRedirect() {
   return <Redirect to={s.authed ? "/home" : "/login"} />;
 }
 
+function AppLayout() {
+  const location = useLocation();
+  const isLogin = location.pathname === "/login";
+
+  return (
+    <IonTabs>
+      <IonRouterOutlet>
+        <Route path="/login" component={LoginPage} exact />
+        <Route path="/home" component={HomePage} exact />
+        <Route path="/lessons" component={LessonsPage} exact />
+        <Route path="/lessons/:id" component={LessonDetail} exact />
+        <Route path="/exercises" component={ExercisesPage} exact />
+        <Route path="/exercises/:id" component={ExerciseDetail} exact />
+        <Route path="/terminal" component={TerminalPage} exact />
+        <Route path="/profile" component={ProfilePage} exact />
+        <Route exact path="/" render={() => <IndexRedirect />} />
+      </IonRouterOutlet>
+      <IonTabBar slot="bottom" hidden={isLogin}>
+        <IonTabButton tab="home" href="/home">
+          <IonIcon icon={homeOutline} />
+          <IonLabel>Inicio</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="lessons" href="/lessons">
+          <IonIcon icon={bookOutline} />
+          <IonLabel>Lecciones</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="exercises" href="/exercises">
+          <IonIcon icon={barbellOutline} />
+          <IonLabel>Ejercicios</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="terminal" href="/terminal">
+          <IonIcon icon={terminalOutline} />
+          <IonLabel>Terminal</IonLabel>
+        </IonTabButton>
+        <IonTabButton tab="profile" href="/profile">
+          <IonIcon icon={personOutline} />
+          <IonLabel>Perfil</IonLabel>
+        </IonTabButton>
+      </IonTabBar>
+    </IonTabs>
+  );
+}
+
 function App() {
   return (
     <IonApp>
       <IonReactRouter>
-        <IonRouterOutlet>
-          <Route path="/login" component={LoginPage} exact />
-          <Route path="/" render={() => (
-            <IonTabs>
-              <IonRouterOutlet>
-                <Route path="/home" component={HomePage} exact />
-                <Route path="/lessons" component={LessonsPage} exact />
-                <Route path="/lessons/:id" component={LessonDetail} exact />
-                <Route path="/exercises" component={ExercisesPage} exact />
-                <Route path="/exercises/:id" component={ExerciseDetail} exact />
-                <Route path="/terminal" component={TerminalPage} exact />
-                <Route path="/profile" component={ProfilePage} exact />
-                <Route exact path="/" render={() => <IndexRedirect />} />
-              </IonRouterOutlet>
-              <IonTabBar slot="bottom">
-                <IonTabButton tab="home" href="/home">
-                  <IonIcon icon={homeOutline} />
-                  <IonLabel>Inicio</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="lessons" href="/lessons">
-                  <IonIcon icon={bookOutline} />
-                  <IonLabel>Lecciones</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="exercises" href="/exercises">
-                  <IonIcon icon={barbellOutline} />
-                  <IonLabel>Ejercicios</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="terminal" href="/terminal">
-                  <IonIcon icon={terminalOutline} />
-                  <IonLabel>Terminal</IonLabel>
-                </IonTabButton>
-                <IonTabButton tab="profile" href="/profile">
-                  <IonIcon icon={personOutline} />
-                  <IonLabel>Perfil</IonLabel>
-                </IonTabButton>
-              </IonTabBar>
-            </IonTabs>
-          )} />
-        </IonRouterOutlet>
+        <AppLayout />
       </IonReactRouter>
     </IonApp>
   );
